788e197f45eea4cc1b31593199638de276bf2720
africamatji@gmail.com <africamatji@gmail.com>
2022-02-14 17:19:03 +0200

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +12,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('{path}', function () {
+    return view('welcome');
+});
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::get('{path}', [HomeController::class, 'index'])->name('welcome')->where('path','.*');
