998f48c834ac080bd54628662541d75e47999550
africamatji@gmail.com <africamatji@gmail.com>
2022-02-15 19:13:29 +0200

    send mail, mailtrap

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -5,6 +5,8 @@ use App\Repositories\ContactsRepo;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
+use App\Mail\MailDispatcher;
+use Illuminate\Support\Facades\Mail;
 use Exception;
 
 class ContactsController extends Controller {
@@ -25,8 +27,10 @@ class ContactsController extends Controller {
 
         try { 
             $response = $contacts->create($request->all());
+            $this->sendMail($request->name, $request->content);
             return response()->json($response);
         } catch(Exception $e) {
+
             return response()->json(['success' => false, 'message' => $e->getMessage()], $e->getCode());
         }   
     }
@@ -41,8 +45,27 @@ class ContactsController extends Controller {
             $response = $contacts->list();
             return response()->json($response);
         } catch(Exception $e) {
+            
             return response()->json(['success' => false, 'message' => $e->getMessage()], $e->getCode());
         }  
     }
+
+    /**
+     * @param String
+     * @param String
+     * @return bool
+    */
+    public function sendMail (String $name, String $content): bool
+    {
+        $details = [
+            'title' => 'Message from : ' . $name,
+            'body' => $content
+        ];
+
+        Mail::to('newuser@example.com')->send(new MailDispatcher($details));
+
+        return true;
+    }
+
 }
 
