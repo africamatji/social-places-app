0fc1273c85fc3e8d4f2a80a9b67648c0a472f86f
africamatji@gmail.com <africamatji@gmail.com>
2022-02-15 19:26:38 +0200

    info

--- resources/js/components/Welcome.vue
+++ resources/js/components/Welcome.vue
@@ -1,7 +1,9 @@
 <template>
   <v-row justify="center">
       <v-col cols="8">
-          About page
+          <h1>Social Places test</h1>
+          <p>A simple contact form application using <strong>Laravel</strong> and <strong>Vue</strong></p>
+          <p>Author : <strong>Africa Matji</strong></p>
       </v-col>
   </v-row>
 </template>
